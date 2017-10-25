38352bd82d18de858619bce267afc85b05c735d6
melodyfs <melodyfs@users.noreply.github.com>
2017-10-25 12:09:35 -0700

    Finish dictionary challenge

--- dictionary_words.py
+++ dictionary_words.py
@@ -1,8 +1,25 @@
 import random
 import sys
 
-with open("/usr/share/dict/words") as f:
-    
+
+def random_words(num):
+    with open("/usr/share/dict/words") as f:
+        original = f.read()
+        split_words = original.split()
+        f.close()
+
+        words = []
+
+        for w in range(0, num):
+            random_num = random.randint(0, len(words))
+            words.append(split_words[random_num])
+        return(" ".join(words))
 
 
-def random_words():
+if __name__ == "__main__":
+    params = sys.argv
+    num = int(params[1])
+    print random_words(num)
+
+
+# random_words()
