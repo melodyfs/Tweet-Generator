d9f0bc6607dfdb9ae6d5f4e7e45b93ecffa08c1b
melodyfs <melodyfs@users.noreply.github.com>
2017-11-09 21:50:54 -0800

    Finish distogram and listogram

--- source/dictogram.py
+++ source/dictogram.py
@@ -19,11 +19,21 @@ class Dictogram(dict):
 
     def add_count(self, word, count=1):
         """Increase frequency count of given word by given count amount."""
-        # TODO: Increase word frequency by count
+        # Increase word frequency by count
+        if word in self:
+            self[word] += count
+        else:
+            self[word] = count
+            self.types += 1
+        self.tokens += 1
 
     def frequency(self, word):
         """Return frequency count of given word, or 0 if word is not found."""
-        # TODO: Retrieve word frequency count
+        # Retrieve word frequency count
+        if word in self:
+            return self[word]
+        return 0
+
 
 
 def print_histogram(word_list):
--- source/listogram.py
+++ source/listogram.py
@@ -19,21 +19,40 @@ class Listogram(list):
 
     def add_count(self, word, count=1):
         """Increase frequency count of given word by given count amount."""
-        # TODO: Increase word frequency by count
+        # Increase word frequency by count
+        if word in self:
+            index = self._index(word)
+            frequency = self[index][1] + count
+            self[index] = (word, frequency)
+        else:
+            self.append((word, count))
+            self.types += 1
+        self.tokens += count
 
     def frequency(self, word):
         """Return frequency count of given word, or 0 if word is not found."""
-        # TODO: Retrieve word frequency count
+        # Retrieve word frequency count
+        for unique_word in self:
+            if unique_word[0] == word:
+                return unique_word[1]
+        return 0
 
     def __contains__(self, word):
         """Return boolean indicating if given word is in this histogram."""
         # TODO: Check if word is in this histogram
+        for unique_word in self:
+            if unique_word[0] == word:
+                return True
+        return False
 
     def _index(self, target):
         """Return the index of entry containing given target word if found in
         this histogram, or None if target word is not found."""
         # TODO: Implement linear search to find index of entry with target word
-
+        for index, word in list(enumerate(self)):
+            if word[0] == target:
+                return index
+        return None
 
 def print_histogram(word_list):
     print('word list: {}'.format(word_list))
